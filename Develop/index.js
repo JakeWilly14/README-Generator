df44e18ee1a9f3e0861a52ac52399ab510e4c921
Jakewilly14 <jakewilly14@gmail.com>
2023-11-14 18:26:47 -0600

    README structure user prompts

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,101 @@
 // TODO: Include packages needed for this application
-
-// TODO: Create an array of questions for user input
-const questions = [];
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const generateReadMe = ({
+  title,
+  description,
+  installation,
+  usage,
+  contributors,
+  test,
+  license,
+  github,
+  email,
+}) =>
+`# ${title}
+
+## Description
+${description}
+## Table of Contents
+*Installation
+*Usage
+*Contributors
+*Test Instructions
+*License
+*Questions
+  
+### Installation
+${installation}
+### Usage
+${usage}
+### Contributors/Credit
+${contributors}
+### Test Instructions
+${test}
+### License
+${license}
+### Questions
+${github}
+${email}`;
+
+// TODO: Create an array of questions for user input
+inquirer
+  .prompts([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of your project?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Enter description of your project.",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "Enter installation instructions for your project.",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Enter usage information for your project.",
+    },
+    {
+      type: "input",
+      name: "contributors",
+      message: "Enter contributors for your project.",
+    },
+    {
+      type: "input",
+      name: "test",
+      message: "Enter test instructions for your project.",
+    },
+    {
+      type: "input",
+      name: "license",
+      message: "Choose a license for your project.",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your Github Username.",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Enter your email address.",
+    },
+  ])
+.then((answers) => {
+    const readMeContent = generateReadMe(answers);
+
+    fs.writeFile('README.md', readMeContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created index.html!')
+    );
+});
 
 // TODO: Create a function to initialize app
 function init() {}
